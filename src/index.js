95ecff651bceb4cf5f4e88b3544e7698e73a7c0f
Giovanni <giovanniadev@gmail.com>
2025-04-09 15:48:31 -0300

    segue projeto com hooks do react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import CyclOPediaClassPage from "./CyclOPediaClassPage";
+import CyclOPediaClassFuncPage from "./CyclOPediaClassFuncPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,10 @@ root.render(
         <div className="col-6">
           <span className="h1 text-warning text-center">Class Component</span>
           <CyclOPediaClassPage />
+        </div>
+        <div className="col-6">
+          <span className="h1 text-warning text-center">Func Component</span>
+          <CyclOPediaClassFuncPage />
         </div>
       </div>
     </div>
